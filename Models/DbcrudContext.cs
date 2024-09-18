30d2741fa02a0dfc5917914683fac0faa04f54c5
Steven1400999 <quezadasteven570@gmail.com>
2024-09-18 17:17:40 -0600

    Before adding an item

---
+++ Models/DbcrudContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+
+namespace WebApiCrud.Models;
+
+public partial class DbcrudContext : DbContext
+{
+    public DbcrudContext()
+    {
+    }
+
+    public DbcrudContext(DbContextOptions<DbcrudContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Empleado> Empleados { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+    { 
+    
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<Empleado>(entity =>
+        {
+            entity.HasKey(e => e.IdEmpleado).HasName("PRIMARY");
+
+            entity.ToTable("empleado");
+
+            entity.Property(e => e.IdEmpleado).HasColumnName("idEmpleado");
+            entity.Property(e => e.Correco).HasMaxLength(100);
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/Empleado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApiCrud.Models;
+
+public partial class Empleado
+{
+    public int IdEmpleado { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public string? Correco { get; set; }
+
+    public int? Sueldo { get; set; }
+}
